bce4961ffc8f0297c6c5f67b83e0c57f71f008ff
Igor Korzhan <igor.human@gmail.com>
2024-05-20 22:27:25 +0300

    Task 03

---
+++ Task03/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+// Делегати.
+
+namespace Delegates
+{
+
+    public delegate int RandomValue();  // 
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Random random = new Random();
+
+            RandomValue[] delegates = new RandomValue[5];
+
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                delegates[i] = delegate ()
+                {
+                    return random.Next(1, 101);
+                };
+            }
+            Func<RandomValue[], double> calculateAverage = delegate (RandomValue[] delArray)
+            {
+                double sum = 0;
+                int count = delArray.Length;
+
+                foreach (var del in delArray)
+                {
+                    sum += del();
+                }
+
+                return sum / count;
+            };
+            // Виклик анонімного методу та виведення результату
+            double average = calculateAverage(delegates);
+            Console.WriteLine($"Середнє арифметичне значення: {average}");
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
